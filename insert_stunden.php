52f8b8c921428c9e1c78385e1f9d4f28da21a695
Huaba01 <138099011+Huaba01@users.noreply.github.com>
2024-02-27 16:47:52 +0100

    f

--- insert_stunden.php
+++ insert_stunden.php
@@ -1,17 +1,13 @@
 <?php
-// Initialize the session
 session_start();
 
-// Database configuration
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "dbArbeitszeitaufzeichnung";
+$dbname = "dbUnternehmen";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
