02ca89c24b4d24858e4c4b883d4edd06d7146141
Huaba01 <138099011+Huaba01@users.noreply.github.com>
2024-03-01 16:07:50 +0100

    fdgh

--- insert_stunden.php
+++ insert_stunden.php
@@ -8,13 +8,20 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$sql_mitarbeiter = "SELECT * FROM tblMitarbeiter";
+$result_mitarbeiter = $conn->query($sql_mitarbeiter);
+
+$sql_projekte = "SELECT * FROM tblProjekte";
+$result_projekte = $conn->query($sql_projekte);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $mstndnMbFID = $_POST["stndnMbFID"];
+    $stndnMbFID = $_POST["stndnMbFID"];
     $projektID = $_POST["projektID"];
     $datum = $_POST["datum"];
-    $start = $_POST["stndnStart"];
-    $end = $_POST["stndnEnd"];
-    $sql = "INSERT INTO tblStunden (stndnMbFID, stndnProjektFID, stndnStart, stndnEnd) VALUES ('$stndnMbFID', '$projektID', '$datum', '$start', '$end')";
+    $start = $_POST["start"];
+    $end = $_POST["end"];
+    $sql = "INSERT INTO tblStunden (stndnMbFID, stndnProjektFID, stndnStart, stndnEnd) VALUES ('$stndnMbFID', '$projektID', '$start', '$end')";
     if ($conn->query($sql) === TRUE) {
         echo "Stunden erfolgreich aufgezeichnet.";
     } else {
@@ -37,21 +44,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="container">       
         <main>
             <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-            <div class="form-group">
-                    <label for="start">Mitarbeiter ID:</label>
-                    <input type="text" class="form-control" id="stndnMbFID" name="stndnMbFID">
+                <div class="form-group">
+                    <label for="stndnMbFID">Mitarbeiter:</label>
+                    <select class="form-control" id="stndnMbFID" name="stndnMbFID">
+                        <?php
+                        if ($result_mitarbeiter->num_rows > 0) {
+                            while ($row = $result_mitarbeiter->fetch_assoc()) {
+                                echo "<option value='" . $row["mbID"] . "'>" . $row["mbName"] . "</option>";
+                            }
+                        }
+                        ?>
+                    </select>
                 </div>
                 <div class="form-group">
-                    <label for="projektID">Projekt ID:</label>
-                    <input type="text" class="form-control" id="projektID" name="projektID">
+                    <label for="projektID">Projekt:</label>
+                    <select class="form-control" id="projektID" name="projektID">
+                        <?php
+                        if ($result_projekte->num_rows > 0) {
+                            while ($row = $result_projekte->fetch_assoc()) {
+                                echo "<option value='" . $row["projektID"] . "'>" . $row["projektName"] . "</option>";
+                            }
+                        }
+                        ?>
+                    </select>
                 </div>
                 <div class="form-group">
                     <label for="start">Startzeit:</label>
-                    <input type="datetime" class="form-control" id="start" name="start">
+                    <input type="datetime-local" class="form-control" id="start" name="start">
                 </div>
                 <div class="form-group">
                     <label for="end">Endzeit:</label>
-                    <input type="datetime" class="form-control" id="end" name="end">
+                    <input type="datetime-local" class="form-control" id="end" name="end">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
